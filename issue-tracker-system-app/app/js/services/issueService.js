c185ad1aed26dfe8469fb2570ef76eedd5eb3de2
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-05-06 22:16:32 +0300

    Dashboard with users's assigned issues

--- issue-tracker-system-app/app/js/services/issueService.js
+++ issue-tracker-system-app/app/js/services/issueService.js
@@ -1,9 +1,23 @@
 'use strict';
 
 app.factory('issueService',
-    function ($http, baseServiceUrl) {
+    function ($http, baseServiceUrl, authService) {
+
+        function getUsersIssues(params, success, error) {
+            var request = {
+                method: 'GET',
+                url: baseServiceUrl + 'issues/me?orderBy=DueDate desc, IssueKey&pageSize=' + params.pageSize + '&pageNumber=' + params.startPage,
+                headers: authService.getAuthHeaders()
+            };
+
+            $http(request).success(function (data) {
+                success(data);
+
+            }).error(error);
+        }
+
         return {
-            //TODO - will need and authService
+            getUsersIssues: getUsersIssues
         }
     }
 );
